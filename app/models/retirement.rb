7d0268e60af911d9b536ace2079562f2f8c30928
Zablon-Onchari-Nyabuto <zablon.nyabuto@student.moringaschool.com>
2023-01-12 13:03:23 +0300

    Update the retirements tables by adding controllers, validations and testing for the code

--- app/models/retirement.rb
+++ app/models/retirement.rb
@@ -1,2 +1,6 @@
 class Retirement < ApplicationRecord
+  validates :years, numericality: { greater_than_or_equal_to: 0 }
+  validates :amount_so_far, numericality: { greater_than_or_equal_to: 0 }
+  validates :goal, numericality: { greater_than: 0 }
+  validates :sources, presence: true
 end
--- test/models/retirement_test.rb
+++ test/models/retirement_test.rb
@@ -1,7 +1,38 @@
 require "test_helper"
 
 class RetirementTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save retirement without years" do
+    retirement = Retirement.new(amount_so_far: 100, goal: 200, sources: 'Salary')
+    assert_not retirement.save, "Saved the retirement without years"
+  end
+
+  test "should not save retirement with negative years" do
+    retirement = Retirement.new(years: -10, amount_so_far: 100, goal: 200, sources: 'Salary')
+    assert_not retirement.save, "Saved the retirement with negative years"
+  end
+
+  test "should not save retirement without amount_so_far" do
+    retirement = Retirement.new(years: 10, goal: 200, sources: 'Salary')
+    assert_not retirement.save, "Saved the retirement without amount_so_far"
+  end
+
+  test "should not save retirement with negative amount_so_far" do
+    retirement = Retirement.new(years: 10, amount_so_far: -100, goal: 200, sources: 'Salary')
+    assert_not retirement.save, "Saved the retirement with negative amount_so_far"
+  end
+
+  test "should not save retirement without goal" do
+    retirement = Retirement.new(years: 10, amount_so_far: 100, sources: 'Salary')
+    assert_not retirement.save, "Saved the retirement without goal"
+  end
+
+  test "should not save retirement with zero goal" do
+    retirement = Retirement.new(years: 10, amount_so_far: 100, goal: 0, sources: 'Salary')
+    assert_not retirement.save, "Saved the retirement with zero goal"
+  end
+
+  test "should not save retirement without sources" do
+    retirement = Retirement.new(years: 10, amount_so_far: 100, goal: 200)
+    assert_not retirement.save, "Saved the retirement without sources"
+  end
 end
