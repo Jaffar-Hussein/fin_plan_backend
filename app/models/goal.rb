6e698f5f4e8faba28585423873f60f41a1f944d5
Zablon-Onchari-Nyabuto <zablon.nyabuto@student.moringaschool.com>
2023-01-12 13:29:36 +0300

    Update the goals tables by adding controllers, validations and testing for the code

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,2 +1,5 @@
 class Goal < ApplicationRecord
+    validates :name, presence: true
+    validates :amount, numericality: { greater_than_or_equal_to: 0 }
+    validates :due_date, presence: true
 end
